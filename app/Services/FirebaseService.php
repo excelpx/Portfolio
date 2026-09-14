83354259385989ac577cd49f6171d1ca46eb68d8
Mr.Ex <100412505+excelpx@users.noreply.github.com>
2026-09-14 20:04:59 +0700

    update

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -10,31 +10,16 @@ class FirebaseService
 
     public function __construct()
     {
-        $factory = new Factory;
-
-        /*
-         * Local:
-         * Menggunakan storage/app/firebase-credentials.json
-         *
-         * Production/Vercel:
-         * Menggunakan FIREBASE_CREDENTIALS_JSON dari Environment Variable
-         */
-
-        $credentialsJson = env('FIREBASE_CREDENTIALS_JSON');
-
-        if ($credentialsJson) {
-            $factory = $factory->withServiceAccount(
-                json_decode($credentialsJson, true)
-            );
-        } else {
-            $factory = $factory->withServiceAccount(
-                storage_path('app/firebase-credentials.json')
-            );
-        }
-
-        $factory = $factory->withDatabaseUri(
-            env('FIREBASE_DATABASE_URL')
+        $credentials = json_decode(
+            env('FIREBASE_CREDENTIALS_JSON'),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
         );
+
+        $factory = (new Factory)
+            ->withServiceAccount($credentials)
+            ->withDatabaseUri(env('FIREBASE_DATABASE_URL'));
 
         $this->database = $factory->createDatabase();
     }
