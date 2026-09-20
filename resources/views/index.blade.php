a06ae85f6e8fd0436981b72c59fc284ae02d5e40
Mr.Ex <100412505+excelpx@users.noreply.github.com>
2026-09-20 17:01:12 +0700

    update title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>EXCEL PETER CHANDRA</title>
+  <title>Portfolio-Excel</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
   <meta name="csrf-token" content="{{ csrf_token() }}">
